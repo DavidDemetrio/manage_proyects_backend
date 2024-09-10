53c07af25584a902eee8f3aa90a5a840f0ce392d
DavidDemetrio <davidlopez.paz26@outlook.es>
2024-09-10 01:03:57 -0600

    Indent correctly shared/logger.ts

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -9,9 +9,7 @@ type Logger = {
 
 const logger : Logger = {
     db: msg => console.log(colors.magenta(msg)),
-    success: msg => {
-        console.log(colors.green(msg))
-    },
+    success: msg => console.log(colors.green(msg)),
     error: msg => console.log(colors.red(msg)),
     warning: msg => console.log(colors.yellow(msg)),
 };
